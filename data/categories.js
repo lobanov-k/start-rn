c1df1722756ef2e6d18295eb4ff17d75bb85a4fe
Kostiantyn Lobanov <lobanov.k.k@gmail.com>
2019-12-23 22:50:05 +0200

    Add categories list data

---
+++ data/categories.js
@@ -0,0 +1,24 @@
+const list = [
+    {
+        name: 'Architecture',
+        alias: 'architecture',
+        collection: '2203755'
+    },
+    {
+        name: 'Food',
+        alias: 'food',
+        collection: '1353633'
+    },
+    {
+        name: 'Abstract',
+        alias: 'abstract',
+        collection: '789734'
+    },
+    {
+        name: 'Pets',
+        alias: 'pets',
+        collection: '2489501'
+    }
+];
+
+export default list;
