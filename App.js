901cea76416be4b92d62a4616ab20035c1261696
Konstantin Lobanov <klobanov@pixsystem.com>
2020-02-04 00:43:37 +0200

    App.js uses navigator

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import CategoriesList from './screens/CategoriesList';
+import Navigator from './navigation';
 
 export default function App() {
   return (
-      <CategoriesList />
+      <Navigator/>
   );
 }
