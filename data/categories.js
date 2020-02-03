2e5303846c33b10fe6e4528c004d522e594bf5f5
Konstantin Lobanov <klobanov@pixsystem.com>
2020-02-04 00:42:42 +0200

    Change collection to url

--- data/categories.js
+++ data/categories.js
@@ -2,22 +2,22 @@ const list = [
     {
         name: 'Architecture',
         alias: 'architecture',
-        collection: '2203755'
+        collection: 'https://firebasestorage.googleapis.com/v0/b/learn-rn-bb59c.appspot.com/o/architecture.json?alt=media&token=b5583dd0-2dbd-4cb9-a91f-76f8e7a0538a'
     },
     {
         name: 'Food',
         alias: 'food',
-        collection: '1353633'
+        collection: 'https://firebasestorage.googleapis.com/v0/b/learn-rn-bb59c.appspot.com/o/food.json?alt=media&token=8b97fcc5-b802-459a-9080-9bd3d49032a6'
     },
     {
         name: 'Abstract',
         alias: 'abstract',
-        collection: '789734'
+        collection: 'https://firebasestorage.googleapis.com/v0/b/learn-rn-bb59c.appspot.com/o/abstract.json?alt=media&token=c7012570-af3f-4f9b-acd9-ad1a9827d837'
     },
     {
         name: 'Pets',
         alias: 'pets',
-        collection: '2489501'
+        collection: 'https://firebasestorage.googleapis.com/v0/b/learn-rn-bb59c.appspot.com/o/pets.json?alt=media&token=f866e7c7-ed4b-46e9-b7c7-ef4b2242b245'
     }
 ];
 
